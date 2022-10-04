fe09fc5640c33d6a02a6c72dde3a86544c2db68c
farraz13 <farraz.f13@gmail.com>
2022-10-04 23:40:16 +0700

    pusing

---
+++ mesinhitung/mesinHitung.mjs
@@ -0,0 +1,53 @@
+export const Pi = 22 / 7;
+
+export default class MesinHitung {
+    constructor(angka) {
+        this.x = 1;
+    }
+
+    add(angka) {
+        this.x += angka;
+        return this;
+        //penjumlahan
+    }
+
+    substract(angka) {
+        this.x -= angka;
+        return this;
+        //pengurangan
+    }
+
+    multiply(angka) {
+        this.x *= angka;
+        return this;
+        //perkalian
+    }
+
+    divide(angka) {
+        this.x /= angka;
+        return this;
+        //pembagian
+    }
+
+    square() {
+        this.x = Math.pow(this.x, 2);
+        return this;
+        // 
+    }
+
+    exponent(angka) {
+        this.x = Math.pow(this.x, angka);
+        return this;
+        //pangkat
+    }
+
+    squareRoot() {
+        this.x = Math.sqrt(this.x);
+        return this;
+        //akar
+    }
+
+    result() {
+        console.log(this.x);
+    }
+}
