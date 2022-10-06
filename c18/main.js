00eb90836086c5cd70f6700cf6759d01eedd6018
farraz13 <farraz.f13@gmail.com>
2022-10-06 22:33:07 +0700

    LastBoss

--- c18/main.js
+++ c18/main.js
@@ -14,8 +14,83 @@ function line() {
     console.log('=======================================================')
 }
 
+function menuMahasiswa(){
+    console.log(`
+    silahkan pilih opsi dibawah ini :
+    [1]Daftar Mahasiswa
+    [2]Cari Mahasiswa
+    [3]Tambah Mahasiswa
+    [4]Hapus Mahasiswa
+    [5]Keluar
+    `);
+    line();
+    rl.question(`masukkan salah satu no. dari opsi diatas`, (opsi) =>{
+        switch(opsi){
+            case'1':
+            DaftarMahasiswa()
+            break;
+            case'2':
+            CariMahasiswa()
+            break;
+            case'3':
+            TambahMahasiswa()
+            break;
+            case'4':
+            HapusMahasiswa()
+            break;
+            case'5':
+            process.exit()
+            break;
+        }
+    })
+}
+function menuJurusan(){
+
+}
+function menuDosen(){
+
+}
+function menuMataKuliah(){
+
+}
+function menuKontrak(){
+
+}
+
 function menuUtama(users){
     console.log(`welcome,${users.username}, your acces level ${users.role.toUpperCase()} `)
+    line()
+    console.log(`
+    silahkan pilih opsi dibawah ini :
+    [1]Mahasiswa
+    [2]Jurusan
+    [3]Dosen
+    [4]Mata kuliah
+    [5]Kontrak
+    [6]Keluar
+    `);
+    line();
+    rl.question(`masukkan salah satu no. dari opsi diatas`, (opsi) =>{
+        switch(opsi){
+            case'1':
+            menuMahasiswa()
+            break;
+            case'2':
+            menuJurusan()
+            break;
+            case'3':
+            menuDosen()
+            break;
+            case'4':
+            menuMataKuliah()
+            break;
+            case'5':
+            menuKontrak()
+            case'6':
+            process.exit()
+            break;
+        }
+    })
 }
 
 function welcome() {
